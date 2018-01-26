e9b060abd338df159a204c75bcab7f107eb657dd
Miha Markovič <mihamarkovic@mihas-macbook-pro.local>
2018-01-26 19:15:03 +0100

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Miha Markovič on 26. 01. 18.
+//  Copyright © 2018 Miha Markovič. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
