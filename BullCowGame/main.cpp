8b6ec6cd8438d0272282e2fdb144547f0ffb3c65
Miha Markovič <mihamarkovic@Mihas-MacBook-Pro.local>
2018-01-28 10:24:07 +0100

    verson 1.0

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -7,9 +7,51 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+void PlayGame();
+string GetGuessAndPrintBack();
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    PrintIntro();
+    PlayGame();
+    
     return 0;
+}
+
+void PrintIntro() {
+    // uvod
+    constexpr int WORLD_LENGTH = 5;
+    cout << "Welcome to bulls and cows" << endl;
+    cout << "Can you guess the world with " << WORLD_LENGTH;
+    cout << " characters" << endl;
+    return;
+}
+
+string GetGuessAndPrintBack() {
+    
+    //vprasaj igralca
+    string Guess = "";
+    cout << "Type your answer here: ";
+    getline(cin, Guess);
+    
+    //odgovori
+    cout << "You typed " << Guess;
+    cout << " in your console" << endl;
+    
+    return Guess;
+}
+
+void PlayGame(){
+    
+    constexpr int NUMBER_OF_TURNS = 5;
+    for(int count = 1; count <= NUMBER_OF_TURNS; count++){
+        GetGuessAndPrintBack();
+        cout << endl;
+    }
+    
 }
